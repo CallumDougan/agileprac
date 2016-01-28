33fbed0f7ca651aa080d679f161eb418ffd8641d
cc2RG <rbgalloway88@gmail.com>
2016-01-28 13:29:40 +0000

    board object and square object created

---
+++ board.js
@@ -0,0 +1,37 @@
+var randomExt = require('random-ext');
+
+var Board = function(){
+
+}
+
+Board.prototype = {
+  generateSquares: function(){
+    var allSquares = [];
+    var gSquare = 0;
+    for(i=0; i <1000; i++){
+      gSquare = new Square(i);
+      allSquares.push(gSquare);
+    }
+    this.squares = allSquares;
+  }
+
+
+}
+
+
+var Square = function(num){
+  this.number = num;
+  this.contents = [];
+
+}
+
+
+Square.prototype = {
+}
+
+module.exports = Board;
+
+
+testBoard = new Board();
+testBoard.generateSquares();
+console.log(testBoard);
--- testing.js
+++ testing.js
@@ -1,7 +1,7 @@
 var chai = require('chai');
 var assert = chai.assert;
 var expect = chai.expect;
-<<<<<<< HEAD
+
 
 
 
@@ -16,4 +16,4 @@ describe('person', function(){
     expect(person.rollDice).to.be.within(1,6)
   })
 })
->>>>>>> 6ea9d29f1c86c30195bd44f726bd0b822f78bb7c
+
